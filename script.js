120acc7114a532c1b8044ad27759ab579ed99065
MuthaiyanarArumugasamy <Priyadharshini23407@gmail.com>
2026-06-28 22:36:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("CalculatorHubs Loaded Successfully");
+
+const searchInput = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("input", function () {
+    const searchText = this.value.toLowerCase();
+
+    cards.forEach((card) => {
+        const title = card.querySelector("h3").textContent.toLowerCase();
+
+        if (title.includes(searchText)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
